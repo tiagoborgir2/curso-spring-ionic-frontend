42178db98c3e935fef035aa24a3b476c915174fe
Tiago Costa <tiago.alcosta@gmail.com>
2021-12-07 16:27:54 -0300

    Carregando produtos de uma dada categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ProdutoService } from './../services/domain/produto.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
@@ -35,8 +36,9 @@ import { AuthInterceptorProvider } from '../interceptors/auth.interceptor';
     AuthInterceptorProvider,
     ErrorInterceptorProvider,
     AuthService,
-    StorageService, 
-    ClienteService
+    StorageService,
+    ClienteService,
+    ProdutoService
   ]
 })
 export class AppModule {}
---
+++ src/services/domain/produto.service.ts
@@ -0,0 +1,15 @@
+import { API_CONFIG } from './../../config/api.config';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs/Rx';
+
+@Injectable()
+export class ProdutoService {
+
+  constructor(public http : HttpClient) {
+  }
+
+  findByCategoria(categoria_id : string) {
+    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+  }
+}
