44771a3e96559d713fc2a2fe3a0d50e6f778b51f
Tiago Costa <tiago.alcosta@gmail.com>
2021-12-10 17:39:52 -0300

    Tela de escolha de pagamento

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -54,7 +54,7 @@ pedido : PedidoDTO;
 
   nextPage(item : EnderecoDTO) {
     this.pedido.enderecoDeEntrega = {id : item.id};
-    console.log(this.pedido);
+    this.navCtrl.push('PaymentPage', {pedido: this.pedido});
   }
 
 }
