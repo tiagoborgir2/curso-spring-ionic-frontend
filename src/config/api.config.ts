6127ff0bd691a502990a8c4692fada305e273264
Tiago Costa <tiago.alcosta@gmail.com>
2021-12-20 15:06:02 -0300

    Heroku up

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -2,3 +2,4 @@ export const API_CONFIG = {
     baseUrl: "http://localhost:8080",
     bucketBaseUrl: "https://curso-spring-ionic-tacosta.s3.sa-east-1.amazonaws.com"
 }
+//baseUrl: "https://curso-spring-ionic-tacosta.herokuapp.com",
