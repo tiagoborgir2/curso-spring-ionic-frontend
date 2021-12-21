bb6f8783caae4458d6fa5e3990d04e5e0345c826
Tiago Costa <tiago.alcosta@gmail.com>
2021-12-21 17:01:42 -0300

    Bugfix - cancelamento da camera ou galeria

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://curso-spring-ionic-tacosta.herokuapp.com",
     bucketBaseUrl: "https://curso-spring-ionic-tacosta.s3.sa-east-1.amazonaws.com"
 }
 //baseUrl: "https://curso-spring-ionic-tacosta.herokuapp.com",
+//baseUrl: "http://localhost:8080",
